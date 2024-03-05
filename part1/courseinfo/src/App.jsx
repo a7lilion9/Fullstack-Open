8574cbb859bd6f1eabfd078dcc70032a20351f56
Ahmed Hlilioune <a7lilion3@gmail.com>
2024-03-05 12:40:54 +0100

    Add exercise 1.2 of courseinfo

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      
+
       <Content part1={part1} part2={part2} part3={part3}
         exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
 
@@ -28,9 +28,9 @@ const Header = (props) => (
 const Content = (props) => {
   return (
     <div>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <Part part={props.part1} exercise={props.exercises1} />
+      <Part part={props.part2} exercise={props.exercises2} />
+      <Part part={props.part3} exercise={props.exercises3} />
     </div>
   )
 }
@@ -39,6 +39,14 @@ const Total = (props) => {
   return (
     <div>
       <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.part} {props.exercise}</p>
     </div>
   )
 }
