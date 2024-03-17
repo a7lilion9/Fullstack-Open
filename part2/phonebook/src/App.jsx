640e13c7e7e65f3162120bba97d8d089f41bf433
Ahmed Hlilioune <a7lilion3@gmail.com>
2024-03-17 03:20:40 +0000

    Complete exercise 2.9 phonebook step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,18 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '1234567890' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
+  const [newSearch, setNewSearch] = useState('')
+
+  const handleSearchChange = e => {
+    setNewSearch(e.target.value)
+  }
 
   const handlePhoneChange = e => {
     setNewPhone(e.target.value)
@@ -36,6 +44,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <p>filter shown with <input value={newSearch} onChange={handleSearchChange} /></p>
+      <h2>Add a new</h2>
       <form onSubmit={addNewName}>
         <div>
           <div>name: <input value={newName} onChange={handleChange} /></div>
@@ -47,7 +57,11 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(p => <li key={p.name}>{p.name} {p.phone}</li>)}
+        {
+          persons
+            .filter(p => p.name.toLowerCase().startsWith(newSearch.toLowerCase()))
+            .map(p => <li key={p.name}>{p.name} {p.number}</li>)
+        }
       </ul>
     </div>
   )
